80512884e84880a6a73328ac8b627044f17758af
HaiTrieu186 <pnhtrieu186@gmail.com>
2026-07-17 21:44:40 +0700

    Init: init project, create .env, docker-compose

---
+++ backend/src/test/java/ptithcm/tttnd35backend/TttnD35BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ptithcm.tttnd35backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TttnD35BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
