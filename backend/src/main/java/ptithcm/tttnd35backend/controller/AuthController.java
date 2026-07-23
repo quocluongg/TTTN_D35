c966e44363bae47c7696bc39561babfb8a4d89f3
HaiTrieu186 <pnhtrieu186@gmail.com>
2026-07-23 09:13:21 +0700

    feat(auth): register API /auth/resend-otp, /auth/verify-otp endpoint in controller

--- backend/src/main/java/ptithcm/tttnd35backend/controller/AuthController.java
+++ backend/src/main/java/ptithcm/tttnd35backend/controller/AuthController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ptithcm.tttnd35backend.dto.request.RegisterRequest;
+import ptithcm.tttnd35backend.dto.request.ResendOtpRequest;
+import ptithcm.tttnd35backend.dto.request.VerifyOtpRequest;
 import ptithcm.tttnd35backend.dto.response.ApiResponse;
 import ptithcm.tttnd35backend.service.IAuthService;
 
@@ -26,6 +28,26 @@ public class AuthController {
         return ResponseEntity.ok(ApiResponse.builder()
                 .success(true)
                 .message("Đăng ký thành công, vui lòng kiểm tra email để lấy mã xác thực")
+                .timestamp(LocalDateTime.now())
+                .build());
+    }
+
+    @PostMapping("/verify-otp")
+    public ResponseEntity<ApiResponse<?>> verifyOtp(@RequestBody @Valid VerifyOtpRequest request) {
+        authService.verifyOtp(request);
+        return ResponseEntity.ok(ApiResponse.builder()
+                .success(true)
+                .message("Xác thực email thành công")
+                .timestamp(LocalDateTime.now())
+                .build());
+    }
+
+    @PostMapping("/resend-otp")
+    public ResponseEntity<ApiResponse<?>> resendOtp(@RequestBody @Valid ResendOtpRequest request) {
+        authService.resendRegisterOtp(request.getEmail());
+        return ResponseEntity.ok(ApiResponse.builder()
+                .success(true)
+                .message("Đã gửi lại mã xác thực, vui lòng kiểm tra email")
                 .timestamp(LocalDateTime.now())
                 .build());
     }
